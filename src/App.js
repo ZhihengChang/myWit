64b1ed40ef2dd7dff202295bc55bbbd10cb77a90
Zhiheng Chang <zhihengchang602@gmail.com>
2021-06-05 18:16:35 -0400

    server: add sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
+import HomePage from './pages/home/homepage.component';
+import SidebarOption from './components/side-bar-option/side-bar-option.component';
+
 function App() {
   return (
     <div>
-      
+      <HomePage />
+      <SidebarOption label='Moment' api='#'/>
     </div>
   );
 }
---
+++ src/pages/home/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import './homepage.styles.css';
+
+const HomePage = () => (
+    <div className = 'homepage'> 
+        Hello
+    </div>
+)
+
+export default HomePage;
